e11e026d2cdf81631c85a6b83e5920c2e6080f33
Nikolay <126760228+Shchelochkov@users.noreply.github.com>
2023-06-27 17:20:36 +0300

    1

---
+++ 3.js
@@ -0,0 +1,36 @@
+
+
+let main = document.getElementById('main');
+let out = document.getElementById('out');
+
+let str = '';
+const nums = [];
+for (let i = 0; i < 25; i++) {
+str += '<div id = '+i+'></div>';
+nums.push(0);
+}
+main.innerHTML = str
+
+const divs = document.querySelectorAll('div');
+console.log(divs);
+for (i in divs) {
+    divs[i].onclick = f1
+}
+
+function f1 () {
+    console.log(this.id);
+    let k = Number(this.id);
+    nums[k]+=1;
+    if (nums[k]>1){nums[k]=0}
+    this.innerHTML = '<img src='+pics[nums[k]]+'>'
+}
+
+function f2(){
+    let num = 0
+    num += 100
+    out.innerHTML = num
+}
+
+
+const pics = ['', 'shar.png']
+
